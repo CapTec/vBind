ac58164d7dbabb89c5c8443be4eea9c0a2a9108b
Daniel Lane <daniel@application-developer.org>
2017-07-24 13:00:46 +0100

    text editor didnt apply formatting correctly

--- test/unit/events.js
+++ test/unit/events.js
@@ -1,74 +1,74 @@
 describe('events', function() {
-	it('registers a subscriber', function() {
-		var subEvtName = 'test',
-			callback = function() {};
+  it('registers a subscriber', function() {
+    var subEvtName = 'test',
+      callback = function() {};
 
-		registered = events.subscribe(subEvtName, callback);
-		expect(events.subscribers[subEvtName]).toContain(callback);
-		events.subscribers = {};
-	});
+    registered = events.subscribe(subEvtName, callback);
+    expect(events.subscribers[subEvtName]).toContain(callback);
+    events.subscribers = {};
+  });
 
-	it('removes a subscriber', function() {
-		var subEvtName = 'test',
-			callback = function() {};
+  it('removes a subscriber', function() {
+    var subEvtName = 'test',
+      callback = function() {};
 
-		registered = events.subscribe(subEvtName, callback);
-		registered.remove();
-		expect(events.subscribers[subEvtName]).not.toContain(callback);
-		events.subscribers = {};
-	});
+    registered = events.subscribe(subEvtName, callback);
+    registered.remove();
+    expect(events.subscribers[subEvtName]).not.toContain(callback);
+    events.subscribers = {};
+  });
 
-	it('clears all subscribers', function() {
-		var subEvtName = 'test',
-			callback = function() {},
-			r0 = events.subscribe(subEvtName, callback),
-			r1 = events.subscribe(subEvtName, callback);
+  it('clears all subscribers', function() {
+    var subEvtName = 'test',
+      callback = function() {},
+      r0 = events.subscribe(subEvtName, callback),
+      r1 = events.subscribe(subEvtName, callback);
 
-		events.clear();
-		expect(events.subscribers).not.toContain(callback);
-		events.subscribers = {};
-	});
+    events.clear();
+    expect(events.subscribers).not.toContain(callback);
+    events.subscribers = {};
+  });
 
-	it('publishes an event', function() {
-		var subEvtName = 'test',
-			callback = jasmine.createSpy('callback spy'),
-			r0 = events.subscribe(subEvtName, callback);
+  it('publishes an event', function() {
+    var subEvtName = 'test',
+      callback = jasmine.createSpy('callback spy'),
+      r0 = events.subscribe(subEvtName, callback);
 
-		events.publish(subEvtName);
-		expect(callback).toHaveBeenCalled();
-		events.subscribers = {};
-	});
+    events.publish(subEvtName);
+    expect(callback).toHaveBeenCalled();
+    events.subscribers = {};
+  });
 
-	it('doesn\'t publish a removed event', function() {
-		var subEvtName = 'test',
-			callback = jasmine.createSpy('callback spy'),
-			r0 = events.subscribe(subEvtName, callback);
+  it('doesn\'t publish a removed event', function() {
+    var subEvtName = 'test',
+      callback = jasmine.createSpy('callback spy'),
+      r0 = events.subscribe(subEvtName, callback);
 
-		r0.remove();
-		events.publish(subEvtName);
-		expect(callback).not.toHaveBeenCalled();
-		events.subscribers = {};
-	});
+    r0.remove();
+    events.publish(subEvtName);
+    expect(callback).not.toHaveBeenCalled();
+    events.subscribers = {};
+  });
 
-	it('doesn\'t trigger a callback that is not subscribed', function() {
-		var subEvtName = 'test',
-			callback = jasmine.createSpy('callback spy');
+  it('doesn\'t trigger a callback that is not subscribed', function() {
+    var subEvtName = 'test',
+      callback = jasmine.createSpy('callback spy');
 
-		events.publish(subEvtName);
-		expect(callback).not.toHaveBeenCalled();
-		events.subscribers = {};
-	});
+    events.publish(subEvtName);
+    expect(callback).not.toHaveBeenCalled();
+    events.subscribers = {};
+  });
 
-	it('published callback value is correct', function() {
-		var subEvtName = 'test',
-			callback = jasmine.createSpy('callback spy'),
-			r0 = events.subscribe(subEvtName, callback),
-			result = {
-				success: true,
-				data: "string data"
-			};
-		events.publish(subEvtName, result);
-		expect(callback).toHaveBeenCalledWith(result);
-		events.subscribers = {};
-	});
+  it('published callback value is correct', function() {
+    var subEvtName = 'test',
+      callback = jasmine.createSpy('callback spy'),
+      r0 = events.subscribe(subEvtName, callback),
+      result = {
+        success: true,
+        data: "string data"
+      };
+    events.publish(subEvtName, result);
+    expect(callback).toHaveBeenCalledWith(result);
+    events.subscribers = {};
+  });
 });
